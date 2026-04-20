036be0a9c4b4cc5f40cee3c580d0eb801be9575a
Alex Brohin <133638105+Alex-brh@users.noreply.github.com>
2026-04-20 12:21:03 -0600

    Add comprehensive end-to-end validation for the Store user flow. (#2)

---
+++ test/specs/store.e2e.ts
@@ -0,0 +1,135 @@
+import HomePage from '../pageobjects/home.page.js';
+import StorePage from '../pageobjects/store.page.js';
+import { $, $$, browser } from '@wdio/globals';
+
+describe('Navigate to "Store" page and', () => {
+
+    beforeEach(async () => {
+        await HomePage.navigateToHomePage()
+        await HomePage.navigateToCustomPage('Store');
+        const currentUrl = await browser.getUrl();
+        expect(currentUrl).toContain('/store');
+        await expect($('a=Best test script A')).toBeDisplayed()
+    });
+
+    it('validate menu items on the top bar of the page', async () => {
+
+        const menuItemDetails = [
+            { index: 0, expectedMenuItemText: 'Home', href: '/' },
+            { index: 1, expectedMenuItemText: 'Store', href: '/store' },
+            { index: 2, expectedMenuItemText: 'FAQ', href: '/faq' },
+            { index: 3, expectedMenuItemText: 'Customer Testimonials', href: '/customer-testimonials' },
+            { index: 4, expectedMenuItemText: 'Contact', href: '/contact' },
+            { index: 5, expectedMenuItemText: 'Elements with frames', href: '/elements-with-frames' },
+            { index: 6, expectedMenuItemText: 'Showcase', href: '/showcase' },
+            { index: 7, expectedMenuItemText: 'Clearance', href: '/clearance' },
+        ];
+
+        for (const menuItem of menuItemDetails) {
+            await HomePage.validateMenuItemsOnTopBar({
+                index: menuItem.index,
+                expectedMenuItemText: menuItem.expectedMenuItemText,
+            });
+            await HomePage.validateMenuItemHrefAttribute({
+                index: menuItem.index,
+                expectedHrefValue: menuItem.href,
+            });
+        }
+    });
+
+    it('validate store products names and links', async () => {
+
+        const productDetails = [
+            { name: 'Best test script A', href: '/product/15277987/best-test-script-a' },
+            { name: 'Best test script B', href: '/product/15278051/best-test-script-b' },
+            { name: 'Best test script C', href: '/product/15278052/best-test-script-c' },
+        ];
+
+        for (const product of productDetails) {
+            const productLink = $(`a=${product.name}`);
+            await productLink.scrollIntoView();
+            await expect(productLink).toBeDisplayed();
+            await expect(productLink).toHaveAttribute('href', product.href);
+        }
+    });
+
+    it('validate store pricing and availability text', async () => {
+
+        const pricingElements = $$('div[class="product__price js-product-container__price"] > span');
+        const pricingElementCount = await pricingElements.length;
+        expect(pricingElementCount).toEqual(3);
+        const prices = ['CA$0.99', 'CA$0.89', 'CA$0.79'];
+
+        for (let i = 0; i < prices.length; i++) {
+            // Check if the pricing elements contain the expected prices in proper order
+            await expect(pricingElements[i]).toHaveText(prices[i]);
+        }
+
+        const availabilityElements = $$('div[class="product-sticker"]');
+        const availabilityElementCount = await availabilityElements.length;
+        expect(availabilityElementCount).toEqual(3);
+
+        for (let i = 0; i < availabilityElementCount; i++) {
+            // Check if the availability elements contain the text "Unavailable"
+            await expect(availabilityElements[i]).toHaveText('Unavailable');
+        }
+    });
+
+    it('validate store action links and footer disclaimer', async () => {
+
+        const viewCartLink = $('a[href="/cart"]');
+        await expect(viewCartLink).toExist();
+        await expect(viewCartLink).toHaveAttribute('href', '/cart');
+
+        const seeDetailsLinks = $$('a*=See details');
+        const seeDetailsLinkCount = await seeDetailsLinks.length;
+        expect(seeDetailsLinkCount).toBeGreaterThanOrEqual(2);
+
+        const disclaimers = $$('div[class="jw-element-imagetext-text"] > p > span');
+        const disclaimerCount = await disclaimers.length;
+        expect(disclaimerCount).toEqual(2);
+
+        for (let i = 0; i < disclaimerCount; i++) {
+            const disclaimerText = await disclaimers[i].getText();
+            expect(disclaimerText).toContain('DISCLAIMER: This is NOT a real');
+            expect(disclaimerText).toContain('educational purposes ONLY');
+        };
+    });
+
+    it('validate each item description content', async () => {
+
+        const productLink = $$('div[class="product__description"] > p');
+        for (let i = 0; i < 4; i++) {
+            expect(productLink[i]).toHaveText(['Description:', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."']);
+        }
+    });
+
+    it('validate that the "Add to cart" buttons are disabled', async () => {
+        const addToCartButtons = $$('button[class^="jw-btn product__add-to-cart"]');
+        const addToCartButtonCount = await addToCartButtons.length;
+        expect(addToCartButtonCount).toEqual(3);
+
+        addToCartButtons.forEach(async (button) => {
+            await expect(button).toBeDisabled();
+            expect(button).toHaveText('Disabled');
+        });
+    });
+
+    it('open each product and validate its details page content', async () => {
+
+        const productLinks = $$('h3[class="product__heading heading__no-margin"] > a[href^="/product/"]');
+        const productLinkCount = await productLinks.length;
+        expect(productLinkCount).toEqual(3);
+
+        const products = [
+            { name: 'Best test script A', urlPart: '/product/15277987/best-test-script-a', price: 'CA$0.99', availability: 'Unavailable' },
+            { name: 'Best test script B', urlPart: '/product/15278051/best-test-script-b', price: 'CA$0.89', availability: 'Unavailable' },
+            { name: 'Best test script C', urlPart: '/product/15278052/best-test-script-c', price: 'CA$0.79', availability: 'Unavailable' },
+
+        ];
+
+        await StorePage.openEachProductAndValidateDetails(products);
+
+    });
+
+});
